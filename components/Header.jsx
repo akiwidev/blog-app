38f8c0d0d75db4b923b34d1b228ee42a29ae77fe
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-12-14 15:43:06 +0900

    replaced hard coded categories with api request to fetch actual categories

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,12 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
-
-const categories = [
-  { name: "Interesting Places", slug: "interesting-places" },
-  { name: "Delicious Eats", slug: "delicious-eats" },
-];
+import { getCategories } from "../services";
 
 const Header = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories));
+  }, []);
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-slate-50 py-8">
