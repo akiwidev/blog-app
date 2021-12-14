2fb6eab33162e26d186f72d3beed401b52fa3783
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-12-14 12:51:58 +0900

    added getSimilarPosts query

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import moment from "moment";
 import Link from "next/link";
 
-const PostWidget = () => {
+import { getRecentPosts } from "../services";
+
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setrelatedPosts] = useState([]);
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(category, slug).then((result) => setrelatedPosts(result));
+    } else {
+      getRecentsPosts().then((result) => setrelatedPosts(result));
+    }
+    return () => {
+      cleanup;
+    };
+  }, [input]);
   return <div>Post Widget</div>;
 };
 
