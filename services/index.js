cfe52d2ee46c42d40a4896158296190323eded8e
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-12-20 11:05:28 +0900

    added submitComment restAPI request to index.js

--- services/index.js
+++ services/index.js
@@ -126,3 +126,12 @@ export const getRecentPosts = async () => {
 
   return result.posts;
 };
+
+export const submitComment = async (obj) => {
+  const result = await fetch("/api/comments", {
+    method: "POST",
+    body: JSON.stringify(obj),
+  });
+
+  return result.json();
+};
