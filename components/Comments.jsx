26ec379eb27edc7f6aaeb2d81e116990ffaf19f4
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-12-21 12:26:10 +0900

    added functionality and jsx to Comments component

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,18 +1,41 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
-import arse from "html-react-parser";
+import parse from "html-react-parser";
+import { getComments } from "../services";
 
 const Comments = ({ slug }) => {
-  const [comment, setComment] = useState([]);
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
-    // getComments(slug)
-    return () => {
-      cleanup;
-    };
+    getComments(slug).then((result) => setComments(result));
   }, []);
 
-  return <div>Comments</div>;
+  return (
+    <>
+      {comments.length > 0 && (
+        <div className="bg-slate-50 shadow-lg rounded-lg p-8 pb-12 mb-8">
+          <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+            {comments.length} Comments
+          </h3>
+          {comments.map((comment) => (
+            <div
+              key={comment.createdAt}
+              className="border-b border-slate-100 mb-4 pb-4"
+            >
+              <p className="mb-4">
+                {" "}
+                <span className="font-semibold">{comment.name}</span> on{" "}
+                {moment(comment.createdAt).format("DD MMM, YYYY")}
+              </p>
+              <p className="whitespace-pre-line text-slate-600 w-full">
+                {parse(comment.comment)}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Comments;
