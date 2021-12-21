107118941bd0dbceb739891a9ac267b87390b8d6
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-12-21 12:10:53 +0900

    imported useState and useEffect into Comments component, and created new getComments query in index.js

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import arse from "html-react-parser";
 
-const Comments = () => {
+const Comments = ({ slug }) => {
+  const [comment, setComment] = useState([]);
+
+  useEffect(() => {
+    // getComments(slug)
+    return () => {
+      cleanup;
+    };
+  }, []);
+
   return <div>Comments</div>;
 };
 
